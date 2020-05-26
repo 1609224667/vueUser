e7f2c17b21176d82eaac7d64e1b280185079c0dd
liuyang-tf <yang_liu@asdc.com.cn>
2020-05-26 09:11:09 +0800

    login ui

--- vue_shop/babel.config.js
+++ vue_shop/babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    [
+      'component',
+      {
+        'libraryName': 'element-ui', // 按需引入的组件库
+        'styleLibraryName': 'theme-chalk' // 按需引入的样式
+      }
+    ]
   ]
 }
--- vue_shop/src/main.js
+++ vue_shop/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(ElementUI)
+import './plugins/element.js'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
 
 Vue.config.productionTip = false
 
