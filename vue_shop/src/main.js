b05947a4d8f1e927caaf9b2fcdba4130198e6ba5
liuyang-tf <yang_liu@asdc.com.cn>
2020-05-27 17:34:15 +0800

    用户列表

--- vue_shop/src/main.js
+++ vue_shop/src/main.js
@@ -13,6 +13,13 @@ import axios from 'axios'
 // 配置请求的跟路径
 // axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+// 请求拦截器，在请求头中添加token
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
